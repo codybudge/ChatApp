622690539d1a6eeb576ff5a5424d75ad73eca5d9
cody budge <codybudge@gmail.com>
2018-09-04 13:30:31 -0600

    moved config to src file and creaded componetDidMount function

--- src/App.js
+++ src/App.js
@@ -5,11 +5,29 @@ import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
+//ChatKit Keys
+import { tokenUrl, instanceLocator } from './config'
+
 class App extends React.Component {
 
 componentDidMount() {
     const chatManager = new  Chatkit.ChatManager({
-        
+        instanceLocator,
+        userId: 'Cody',
+        tokenProvider: new Chatkit.TokenProvider({
+            url: tokenUrl
+        })
+    })
+    chatManager.connect()
+    .then(currentUser => {
+        currentUser.subscribeToRoom({
+            roomId: 15429101,
+            hooks: {
+                onNewMessage: message => {
+                    console.log('message.text: ', message.text);
+                }
+            }
+        })
     })
 };
 
