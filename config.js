35e27defbb24484c8f2d3d918429a443b2450852
cody budge <codybudge@gmail.com>
2018-09-04 13:11:00 -0600

    added config with ChatKit Data

---
+++ config.js
@@ -0,0 +1,5 @@
+const tokenUrl =
+"https://us1.pusherplatform.io/services/chatkit_token_provider/v1/c57e6056-3dd5-4acd-86fc-2da1b4262ab3/token";
+
+const instanceLocator = "v1:us1:c57e6056-3dd5-4acd-86fc-2da1b4262ab3"
+export { tokenUrl, instanceLocator}
